7f2a35c2c8da6d312e51bfd84b5e0f46fa634b4f
Vens <v.kolev@alfatrex.com>
2022-04-20 14:39:51 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { join, dirname } from "path";
 import express from "express";
 const app = express();
+
 import { Low, JSONFile } from "lowdb";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,32 +11,61 @@ const file = join(__dirname, "/data/db.json");
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
-const defaultData = {
-  cities: [
-    {
-      id: 1,
-      name: "Hamburg",
-      inhabitants: 1800000
-    },
-    {
-      id: 2,
-      name: "Köln",
-      inhabitants: 1086000
-    },
-    {
-      id: 3,
-      name: "Berlin",
-      inhabitants: 4000000
-    }
-  ]
+const setUpDb = async (db) => {
+  const defaultData = {
+    cities: [
+      {
+        id: 1,
+        name: "Hamburg",
+        inhabitants: 1800000
+      },
+      {
+        id: 2,
+        name: "Köln",
+        inhabitants: 1086000
+      },
+      {
+        id: 3,
+        name: "Berlin",
+        inhabitants: 4000000
+      }
+    ]
+  };
+  if (db.data === null) {
+    db.data = defaultData;
+    await db.write();
+  }
 };
 
-db.data ||= defaultData;
-await db.write();
+setUpDb(db);
+app.use(express.json());
+app.get("/", async (req, res) => {
+  await db.read();
+  res.send(db.data);
+});
 
-app.get("/", (req, res) => {
-  db.read();
-  res.send(db.data.cities);
+app.get("/cities/:name", async (req, res) => {
+  await db.read();
+  console.log("Param name: ", req.params.name);
+  const city = db.data.cities.find((city) => city.name === req.params.name);
+  console.log(city);
+  res.send(city);
+});
+
+app.patch("/cities/:id", async (req, res) => {
+  await db.read();
+  const updateDate = req.body;
+  // console.log(req.body);
+  // console.log(req.params.id);
+  const cityIndex = db.data.cities.findIndex(
+    (city) => city.id === parseInt(req.params.id)
+  );
+  console.log(cityIndex);
+  db.data.cities[cityIndex] = { ...db.data.cities[cityIndex], ...updateDate };
+  // db.data.cities.push({ ...city, ...updateDate });
+  // city = { ...city, ...updateDate };
+  await db.write();
+  res.send(db.data.cities[cityIndex]);
 });
 
 app.listen(3000, function () {
